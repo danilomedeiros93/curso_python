cba23819dbb6b34be87c25b48594d1e49a5a8275
danilomedeiros93 <danilo.medeirosleite@gmail.com>
2025-04-19 15:01:10 -0300

    commit

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
 caminho_arquivo = 'C:\\Users\\USER\\Desktop\\atencao_aula_arquivos\\'
-caminho_arquivo += ''
+caminho_arquivo += 'exercicio_arquivo.txt'
 
-arquivo = open(caminho_arquivo, 'w')
+# arquivo = open(caminho_arquivo, 'w')
 
-arquivo.close()
+# arquivo.close()
+
+with open(caminho_arquivo,'w', encoding='utf8') as arquivo:
+    arquivo.write('Atenção \n')
+    arquivo.write('teste \n')
+    arquivo.write('teste \n')
